4bfd6ec51847ed619252263ade1f209967d359ac
yfernando <yohanfernando@gmail.com>
2023-05-22 04:15:17 +0100

    Create subnets in each AZ for HA and associate routing for IGW and NATGW

---
+++ notes.md
@@ -0,0 +1,4 @@
+
+
+# Add subnets in each AZ
+https://www.bogotobogo.com/DevOps/Terraform/Terraform-VPC-Subnet-ELB-RouteTable-SecurityGroup-Apache-Server-1.php
--- variables.tf
+++ variables.tf
@@ -1,4 +1,22 @@
-variable "region" {}
-variable "main_vpc_cidr" {}
-variable "public_subnet" {}
-variable "private_subnet" {}
+variable "region" {
+    default = "eu-west-1"
+}
+
+variable "main_vpc_cidr" {
+    default = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+    type = list
+    default = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
+}
+
+variable "private_subnets" {
+    type = list
+    default = ["10.0.11.0/24","10.0.22.0/24","10.0.33.0/24"]
+}
+
+variable "azs" {
+    type = list
+    default = ["eu-west-1a","eu-west-1b","eu-west-1c"]
+}
